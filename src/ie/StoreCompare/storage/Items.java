118e2554de9779aed6cdacc9dafa17de1f527a1f
Cian Gannon <lego@live.ie>
2018-04-23 16:49:33 +0100

    Commented item object

--- src/ie/StoreCompare/storage/Items.java
+++ src/ie/StoreCompare/storage/Items.java
@@ -9,31 +9,38 @@ package ie.StoreCompare.storage;
  */
 public class Items {
 
+	// Variables used
 	private String name;
 	private double price;
 
+	// Set instance
 	public Items(String name, double price) {
 		super();
 		this.name = name;
 		this.price = price;
 	}
 
+	// Return name instance
 	public String getName() {
 		return name;
 	}
 
+	// Set name instance
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	// Get price instance
 	public double getPrice() {
 		return price;
 	}
 
+	// Set price instance
 	public void setPrice(double price) {
 		this.price = price;
 	}
 
+	// Return instance data
 	@Override
 	public String toString() {
 		return "\nName:  " + name + "\nPrice: €" + price + "\n";
