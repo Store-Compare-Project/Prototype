4efaef940ca1b45b03679bfc81f946a2da1e50eb
cian2009 <g00337022@gmit.ie>
2018-04-23 14:09:50 +0100

    Added javadocs to runner

--- src/ie/StoreCompare/runner/Runner.java
+++ src/ie/StoreCompare/runner/Runner.java
@@ -3,7 +3,7 @@ package ie.StoreCompare.runner;
 import ie.StoreCompare.core.Menu;
 
 /**
- * The runner responisble for kick starting the program
+ * The runner responsible for kick starting the program.
  * 
  * @author Cian Gannon
  * @author Danielis Joniškis
@@ -11,8 +11,18 @@ import ie.StoreCompare.core.Menu;
  */
 public class Runner {
 
+	/**
+	 * 
+	 * This method calls the 'Menu.main' function which starts the user
+	 * interface.
+	 * 
+	 * @param args
+	 *            Default method argument
+	 * @throws InterruptedException
+	 *             Stops the threads from crashing the program
+	 */
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		Menu.main(args);
 
 	}
